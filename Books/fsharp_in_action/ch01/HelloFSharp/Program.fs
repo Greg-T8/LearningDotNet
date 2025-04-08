5a70ab83fa7cb3ffaa6839d62c404019b4640c23
Greg Tate <gregt8@outlook.com>
2025-04-08 04:00:30 -0500

    Introduce personalized greeting in Program.fs with name and current time

--- Books/fsharp_in_action/ch01/HelloFSharp/Program.fs
+++ Books/fsharp_in_action/ch01/HelloFSharp/Program.fs
@@ -1,2 +1,3 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿let name = "Greg"
+let time = System.DateTime.UtcNow
+printfn $"Hello from F#! My name is {name}, the time is {time}"
