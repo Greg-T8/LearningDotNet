97df21548b5e78db5d8abad9c20882c14753650d
Greg Tate <gregt8@outlook.com>
2025-04-08 04:02:06 -0500

    Add troubleshooting notes for Ionide F# extension issues in Program.fs

--- Books/fsharp_in_action/ch01/HelloFSharp/Program.fs
+++ Books/fsharp_in_action/ch01/HelloFSharp/Program.fs
@@ -1,3 +1,6 @@
 ﻿let name = "Greg"
 let time = System.DateTime.UtcNow
 printfn $"Hello from F#! My name is {name}, the time is {time}"
+
+// When picking back up, note that the Ionide for F# extension created issues;
+// need to troubleshoot.
