a611eb96c6500cbdf2107626fbcac39d48cf2099
Greg Tate <gregt8@outlook.com>
2025-04-28 04:01:58 -0500

    Ch02: Add examples for decimal, binary, and hexadecimal notation in Program.cs

--- Books/price_development_fundamentals/ch02/Numbers/Program.cs
+++ Books/price_development_fundamentals/ch02/Numbers/Program.cs
@@ -14,3 +14,22 @@ float realNumber = 2.3f;
 // A double is a double-precision floating-point number
 // double is the default type for floating-point literals
 double anotherRealNumber = 2.3; // A double literal value
+
+
+int decimalNotation = 2_000_000; // Note use of digit separator for readability
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+// Check the three variables have the same value
+Console.WriteLine($"{decimalNotation == binaryNotation}"); // True
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}"); // True
+
+// Output the variable values in decimal.
+Console.WriteLine($"{decimalNotation:N0}"); // 2,000,000
+Console.WriteLine($"{binaryNotation:N0}"); // 2,000,000
+Console.WriteLine($"{hexadecimalNotation:N0}"); // 2,000,000
+
+// Output the variable values in hexadecimal.
+Console.WriteLine($"{decimalNotation:X}"); // 1E8400
+Console.WriteLine($"{binaryNotation:X}"); // 1E8400
+Console.WriteLine($"{hexadecimalNotation:X}"); // 1E8400
