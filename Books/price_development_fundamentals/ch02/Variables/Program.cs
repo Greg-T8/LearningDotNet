4f15ef4d9e01df49bca34a0e4c23313b38fd1969
Greg Tate <gregt8@outlook.com>
2025-05-07 03:40:47 -0500

    Ch02: Update Program.cs to demonstrate dynamic object usage with arrays and type casting

--- Books/price_development_fundamentals/ch02/Variables/Program.cs
+++ Books/price_development_fundamentals/ch02/Variables/Program.cs
@@ -1,6 +1,18 @@
-﻿object height  = 1.88;                   // storing a double in an object
-object name    = "Amir";                 // storing a string in an object
-Console.WriteLine($"{name} is {height} meteres tall.");
-// int    length1 = name.Length;            // compiler error: name is an object, not a string
-int    length2 = ((string)name).Length;  //Casting the object to a string
-Console.WriteLine($"{name} has {length2} characters.");
+﻿dynamic something;
+// Storing an array of int values in a dynamic object.
+// An array of any type has a Length property.
+something = new[] { 3, 5, 7 };
+
+// Storing an int in a dynamic object.
+// int does not have a Length property.
+// something = 12;
+
+// Storing a string in a dynamic object.
+// string has a Length property.
+// something = "Ahmed";
+
+// This compiles, but might throw a runtime exception if something is not a string.
+Console.WriteLine($"The length of something is {something.Length}.");
+
+// Ouptut the type of the something variable.
+Console.WriteLine($"The type of something is {something.GetType()}.");
