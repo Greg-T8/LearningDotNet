bd072dd58d3c9ad6b83b5672ec29feb53a7188bb
Greg Tate <gregt8@outlook.com>
2025-05-07 03:23:23 -0500

    Ch02: Add Variables project and example code for object usage

---
+++ Books/price_development_fundamentals/ch02/Variables/Program.cs
@@ -0,0 +1,6 @@
+﻿object height  = 1.88;                   // storing a double in an object
+object name    = "Amir";                 // storing a string in an object
+Console.WriteLine($"{name} is {height} meteres tall.");
+// int    length1 = name.Length;            // compiler error: name is an object, not a string
+int    length2 = ((string)name).Length;  //Casting the object to a string
+Console.WriteLine($"{name} has {length2} characters.");
