47d51ffef7c6dd0d36e654923b11c7c3a60e081b
Greg Tate <gregt8@outlook.com>
2025-06-13 03:58:46 -0500

    Ch02: Add TextInput project with basic user input functionality

---
+++ Books/price_development_fundamentals/ch02/TextInput/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Type your first name and press ENTER: ");
+string? firstName = Console.ReadLine();                             // Use of the nullable refernce type annotation
+Console.Write("Type your age and press ENTER: ");
+string age = Console.ReadLine()!;                                   // Use of the null-forgiving operator
+Console.WriteLine($"Hello {firstName}, you look good for {age}.");
