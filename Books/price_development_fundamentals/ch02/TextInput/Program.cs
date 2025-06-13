cb9a922fba6e5837a4999c4c10e9aa3bbcda2d25
Greg Tate <gregt8@outlook.com>
2025-06-13 04:45:34 -0500

    Ch02: Add Arguments project and update solution file; modify TextInput project for key input functionality

--- Books/price_development_fundamentals/ch02/TextInput/Program.cs
+++ Books/price_development_fundamentals/ch02/TextInput/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.Write("Type your first name and press ENTER: ");
-string? firstName = Console.ReadLine();                             // Use of the nullable refernce type annotation
-Console.Write("Type your age and press ENTER: ");
-string age = Console.ReadLine()!;                                   // Use of the null-forgiving operator
-Console.WriteLine($"Hello {firstName}, you look good for {age}.");
+﻿Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+    arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);
