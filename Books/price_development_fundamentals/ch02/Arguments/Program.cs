b89ec6bde1b89c47f678970da176777b3fb0c8ba
Greg Tate <gregt8@outlook.com>
2025-06-15 05:00:07 -0500

    Ch02: Update argument validation in Program.cs to ensure correct input format

--- Books/price_development_fundamentals/ch02/Arguments/Program.cs
+++ Books/price_development_fundamentals/ch02/Arguments/Program.cs
@@ -1,4 +1,23 @@
-﻿foreach (string arg in args)
+﻿if (args.Length < 3)
 {
-    WriteLine(arg);
+    WriteLine("You must specify two colors and a cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return;
+}
+
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0], ignoreCase: true);
+
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1], ignoreCase: true);
+
+try
+{
+    CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+    WriteLine("The current platform does not support changing the cursor size.");
 }
