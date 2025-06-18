f98d8f2c70f618696b2bea5b108a7073f4a8f2bb
Greg Tate <gregt8@outlook.com>
2025-06-18 04:05:37 -0500

    Ch03: Notes: Update omitted ResizeArray example to demonstrate type inference with a string

--- Books/fsharp_in_action/ch03/ResizeArray.fsx
+++ Books/fsharp_in_action/ch03/ResizeArray.fsx
@@ -1,6 +1,6 @@
-let explicit = ResizeArray<int>()       // Creates a resizable array of ints with an explicit generic type argument
-let typeHole = ResizeArray<_>()         // Creates a resizable array of ints using the underscore as a typeHole
-let omitted = ResizeArray()             // Creates a resizable array of ints omitting the generic argument completely
+let explicit = ResizeArray<int>()
+let typeHole = ResizeArray<_>()
+let omitted = ResizeArray()
 
 typeHole.Add 99
-omitted.Add 10
+omitted.Add "abc"
