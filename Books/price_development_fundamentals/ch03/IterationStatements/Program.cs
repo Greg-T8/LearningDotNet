bc1550c50805aa892dccd9773536f5eaa2ed04a7
Greg Tate <gregt8@outlook.com>
2025-07-13 04:28:18 -0500

    Ch03: Notes: Add IterationStatements project and implement while and do statements in Program.cs

---
+++ Books/price_development_fundamentals/ch03/IterationStatements/Program.cs
@@ -0,0 +1,22 @@
+﻿#region while statement
+int x = 0;
+while (x < 10)
+{
+    WriteLine(x);
+    x++;
+}
+#endregion
+
+#region do statement
+
+string? actualPassword = "Pa$$w0rd";
+string? password;
+do
+{
+    Write("Enter your password: ");
+    password = ReadLine();
+}
+while (password != actualPassword);
+WriteLine("Correct!");
+
+#endregion
