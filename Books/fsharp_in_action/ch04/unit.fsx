9d85a6de2f8e22ab98c1c14c74d856928c709b35
Greg Tate <gregt8@outlook.com>
2025-07-16 04:01:00 -0500

    Ch04: Notes: Add unit testing functions for addition and date manipulation

---
+++ Books/fsharp_in_action/ch04/unit.fsx
@@ -0,0 +1,20 @@
+let printAddition a b =
+    let answer = a + b
+    printfn $"{a} plus {b} is {answer}."
+
+
+let getTheCurrentTime = System.DateTime.Now         // Calculates the current time and assigns it to a symbo
+let x = getTheCurrentTime                           // Copies the value of getCurrentTime to another symbol
+let y = getTheCurrentTime                           // Copies the value of getCurrentTime to another symbol
+
+
+// let getTheCurrrentTime () = System.DateTime.Now
+// let x = getTheCurrrentTime ()                       // Calls the function to get the current time
+// let y = getTheCurrrentTime ()                       // Calls the function to get the current time again
+
+
+let addDays days =
+    let newDays = System.DateTime.Today.AddDays days
+    printfn $"You gave me {days} days and I gave you {newDays}."
+    newDays
+let result = addDays 3
